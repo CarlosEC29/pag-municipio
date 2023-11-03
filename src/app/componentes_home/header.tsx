d669a49234590f3df6bf6b1ba2f10888de9ee11f
gonzalo <gonzalogabrielapu@gmail.com>
2023-11-03 15:23:49 -0300

    carpetas-gonzalo

---
+++ src/app/componentes_home/header.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+export const Titulo = () => {
+  return (
+    <main>
+      <div className='titulo'>pagina principal</div>
+      <p className='cuerpo'> titulo</p>
+    </main>
+
+  )
+}
---
+++ src/app/contratos.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import {Titulo} from './componentes_home/header'
+
+export default function Home() {
+  return (
+    <main>
+      <div> 
+		titulo
+        
+      </div>
+      <div>
+        cuerpo
+
+      </div>
+      <footer>
+        pie de pagina
+
+      </footer>
+    </main>
+    
+  )
+}
