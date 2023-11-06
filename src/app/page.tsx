590bc5acef2a283d4d9e473d78b88cacb3a000c4
Luchxx013 <lucho31_2002@hotmail.com>
2023-11-05 21:56:04 -0300

    Avance Postulaciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import '../app/layout'
 import { Titulo } from './componentes_home/header'
+import { Postulaciones } from './postulaciones'
+
+
+/*export default function Home() {
+  return (
+    <main>
+	  <Titulo/>
+    </main>
+  )
+}*/
 
 export default function Home() {
   return (
     <main>
-	  <Titulo/>
+	  <Postulaciones/>
     </main>
   )
 }
--- src/app/postulaciones.tsx
+++ src/app/postulaciones.tsx
@@ -1,23 +1,56 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-import {Titulo} from './componentes_home/header'
+import { Titulo } from './componentes_home/header'
 
-export default function Home() {
+interface TarjetaProps {
+  titulo: string;
+  descripcion: string;
+}
+
+function Tarjeta({ titulo, descripcion }: TarjetaProps) {
   return (
-    <main>
-      <div> 
-		titulo
-        
-      </div>
-      <div>
-        cuerpo
 
-      </div>
-      <footer>
-        pie de pagina
+    <div className="tarjeta">
+      <h2>{titulo}</h2>
+      <p>{descripcion}</p>
+      <button>Cargar CV</button>
+    </div>
+  );
+}
 
-      </footer>
-    </main>
-    
+interface ListaDeTarjetasProps {
+  datosTarjetas: TarjetaProps[];
+}
+
+function ListaDeTarjetas({ datosTarjetas }: ListaDeTarjetasProps) {
+  return (
+    <div className="lista-de-tarjetas">
+      {datosTarjetas.map((tarjeta, index) => (
+        <Tarjeta key={index} titulo={tarjeta.titulo} descripcion={tarjeta.descripcion} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const alertaCV = (event: React.MouseEvent<HTMLElement>) => {
+    alert("CV CARGADO!");
+  };
+  
+  const tarjetas: TarjetaProps[] = [
+    { titulo: 'Trabajo 1', descripcion: 'Esta es la primera tarjeta.' },
+    { titulo: 'Trabajo 2', descripcion: 'Esta es la segunda tarjeta.' },
+    { titulo: 'Trabajo 3', descripcion: 'Esta es la tercera tarjeta.' },
+  ];
+
+  return (
+    <div className="App">
+      <h1>Lista de trabajos</h1>
+      <ListaDeTarjetas datosTarjetas={tarjetas} />
+    </div>
+  );
+}
+export const Postulaciones = () => {
+  return (
+    App()
   )
 }
