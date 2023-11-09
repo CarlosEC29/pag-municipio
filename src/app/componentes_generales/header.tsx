a2e14a727315b30d0582203da21a577c26711980
gonzalo <gonzalogabrielapu@gmail.com>
2023-11-09 18:10:49 -0300

    actualizacion

--- src/app/componentes_generales/header.tsx
+++ src/app/componentes_generales/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import { Link } from 'react-router-dom'
+//import {Icono} from './icono'
 
 export const Header = () => {
   return (
@@ -11,12 +12,11 @@ export const Header = () => {
             <li><a href="#">Home</a></li>
             <li><a href="#">Postulaciones</a></li>
             <li><a href="#">Contrataciones</a></li>
-            <li><a href="#" target="blank"></a></li>
-            <li><a href="#"
-              target="blank"></a></li>
+            <li><a href="#">Buscador</a></li>
           </ul>
         </nav>
       </header>
+      
     </section>
   )
 }
