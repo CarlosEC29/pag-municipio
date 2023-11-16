9db175ec22759ef74c605fb4059313290322e220
gonzalo <gonzalogabrielapu@gmail.com>
2023-11-16 19:04:28 -0300

    orden de botones

--- src/app/(componentes)/generales/header.tsx
+++ src/app/(componentes)/generales/header.tsx
@@ -18,8 +18,8 @@ export const Header = () => {
         <nav>
           <ul>
             <li><a onClick={goHome}>Home</a></li>{/*con el onClick llamo a la funcion goHome  para habrir la pagina */}
-            <li><a href="/postulaciones">Empleos</a></li>{ /* con un href llamo a la carpeta para habrirlo al ocar el boton */ }
-            <li><a onClick={goContrataciones} >Candidatos</a></li>
+            <li><a onClick={goContrataciones}>Empleos</a></li>
+            <li><a href="/postulaciones" >Candidatos</a></li>{ /* con un href llamo a la carpeta para habrirlo al ocar el boton */ }
             <li><BarraBusqueda/></li>{/*llamo a la funcion barra de busqueda para mostrarla en pantalla */}
           </ul>
         </nav>
