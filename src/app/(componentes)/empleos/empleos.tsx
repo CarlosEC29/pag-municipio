2aeba94a739c267e96f5c62fc6cc32c1ddaa3b2c
gonzalo <gonzalogabrielapu@gmail.com>
2023-11-30 18:09:09 -0300

    desplegables en empleos

--- src/app/(componentes)/empleos/empleos.tsx
+++ src/app/(componentes)/empleos/empleos.tsx
@@ -8,6 +8,7 @@ import MascotaImage from '../../../../public/mascota.png'
 import RunasImage from '../../../../public/runas.png'
 import DefensasImage from '../../../../public/defensa oscura.png'
 import { useRouter } from 'next/navigation'
+import Accordion from 'react-bootstrap/Accordion';
 
 export const Body = () => {
   const router = useRouter(); {/*funcion para llamar al router */ }
@@ -21,45 +22,63 @@ export const Body = () => {
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={RunasImage.src} />
           <Card.Body>
-            <Card.Title>Estudios de Runas Antiguas</Card.Title>
-            <Card.Text>
-              El Estudio de Runas Antiguas (comúnmente abreviado Runas Antiguas) es un curso electivo en el Colegio Hogwarts de Magia y Hechicería y presumiblemente el Colegio Ilvermorny de Magia y Hechicería, que pueden ser tomadas por los estudiantes de tercer año en adelante.
-            </Card.Text>
-            <small className="text-muted">cargado hace 2 dias<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+            <Accordion defaultActiveKey="0" flush >
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Estudios de Runas Antiguas</Accordion.Header>
+                <Accordion.Body>
+                  El Estudio de Runas Antiguas (comúnmente abreviado Runas Antiguas) es un curso electivo en el Colegio Hogwarts de Magia y Hechicería y presumiblemente el Colegio Ilvermorny de Magia y Hechicería, que pueden ser tomadas por los estudiantes de tercer año en adelante.
+                  <small className="text-muted">cargado hace 2 dias<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Card.Body>
         </Card>
+
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={EstudiosImage.src} />
           <Card.Body>
-            <Card.Title>Estudios Muggles</Card.Title>
-            <Card.Text>
-              Estudios Muggles (tambien llamada Ciencias Muggles) es una asignatura opcional y parte del plan de estudios no mágicos en el Colegio Hogwarts de Magia y Hechicería. Consiste en el estudio de la historia y la vida cotidiana de los muggles, y cómo podían vivir sin magia, sino con electricidad, tecnología y ciencia
-            </Card.Text>
-            <small className="text-muted">cargado hace 2 horas<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+            <Accordion defaultActiveKey="0" flush >
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Estudios Muggles</Accordion.Header>
+                <Accordion.Body>
+                  Estudios Muggles (tambien llamada Ciencias Muggles) es una asignatura opcional y parte del plan de estudios no mágicos en el Colegio Hogwarts de Magia y Hechicería. Consiste en el estudio de la historia y la vida cotidiana de los muggles, y cómo podían vivir sin magia, sino con electricidad, tecnología y ciencia
+                  <small className="text-muted">cargado hace 2 horas<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Card.Body>
         </Card>
+
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={MascotaImage.src} />
           <Card.Body>
-            <Card.Title>Cuidado de Criaturas Magicas</Card.Title>
-            <Card.Text>
-            Cuidado de Criaturas Mágicas es un curso electivo del Colegio Hogwarts de Magia y Hechicería, que puede ser elegido por los estudiantes en su tercer año. A los estudiantes se les enseña acerca de la alimentación, mantenimiento, la cría y el tratamiento adecuado de estas criaturas y muchas más
-            </Card.Text>
-            <small className="text-muted">cargado hace 3 semanas<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+            <Accordion defaultActiveKey="0" flush >
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Cuidado de Criaturas Magicas</Accordion.Header>
+                <Accordion.Body>
+                  Cuidado de Criaturas Mágicas es un curso electivo del Colegio Hogwarts de Magia y Hechicería, que puede ser elegido por los estudiantes en su tercer año. A los estudiantes se les enseña acerca de la alimentación, mantenimiento, la cría y el tratamiento adecuado de estas criaturas y muchas más
+                  <small className="text-muted">cargado hace 3 semanas<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Card.Body>
         </Card>
+
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={DefensasImage.src} />
           <Card.Body>
-            <Card.Title>Defensas de las artes oscuras</Card.Title>
-            <Card.Text>
-            En esta clase, los estudiantes estudian y aprenden cómo defenderse de todos los aspectos de las Artes Oscuras, incluidas las criaturas oscuras.
-            </Card.Text>
-            <small className="text-muted">cargado hace 3 semanas<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+            <Accordion defaultActiveKey="0" flush>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Defensas de las artes oscuras</Accordion.Header>
+                <Accordion.Body>
+                  En esta clase, los estudiantes estudian y aprenden cómo defenderse de todos los aspectos de las Artes Oscuras, incluidas las criaturas oscuras.
+                  <small className="text-muted">cargado hace 3 semanas<Button onClick={goFormulario} variant="success" size="sm">Cargar CV</Button></small>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Card.Body>
         </Card>
       </CardGroup>
     </section>
-
   )
 }
