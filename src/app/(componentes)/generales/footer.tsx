395c0dbff2dbf69916cb250ee98a5a224f4dbf96
gonzalo <gonzalogabrielapu@gmail.com>
2023-12-06 20:08:17 -0300

    correcion del footer general

--- src/app/(componentes)/generales/footer.tsx
+++ src/app/(componentes)/generales/footer.tsx
@@ -5,8 +5,8 @@ export const Footer = () => {
   return (
     <footer className="finpagina">{/*le agrego el css */}
       <div className="container">
-        <p className="float-start"> contacto telefonico: 2284-****** </p>
-        <p className="float-end">correo:<a className="link-opacity-50-hover"
+        <p className="float-start"> Contacto Telefonico: 0-800-Hodwarts-111-999 </p>
+        <p className="float-end">Correo:<a className="link-opacity-50-hover"
           href="" target="blank">municipalidadhodward@gmail.com</a></p>{/*al correo lo hago un boton link */}
       </div>
     </footer>
