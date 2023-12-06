6a9c9ed0755bdcc4cbab90bbee244a53a1f2f4c4
gonzalo <gonzalogabrielapu@gmail.com>
2023-12-06 11:23:56 -0300

    correccion de buscador

--- src/app/(componentes)/barraBusqueda/BarraBusqueda.tsx
+++ src/app/(componentes)/barraBusqueda/BarraBusqueda.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const BarraBusqueda = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg ">
       <div className="container">
         <button
           className="navbar-toggler"
@@ -20,16 +20,6 @@ export const BarraBusqueda = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Menu
-              </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li><a className="dropdown-item" href="#">Empleos Disponibles</a></li>
                 <li><a className="dropdown-item" href="#">Carga de CVs</a></li>
--- src/app/(componentes)/generales/barraMenuLateral/barraLateral.tsx
+++ src/app/(componentes)/generales/barraMenuLateral/barraLateral.tsx
@@ -10,7 +10,7 @@ export const BarraLateral = () => {
     <div className=" izquierda">{/*le agrego el css */}
 
       <Nav defaultActiveKey="/home" className="flex-column"> {/* */}
-        <nav className='mb-3'><a className='page-link boton2' href=''> Twitter</a></nav>
+        <nav className='mb-3  boton1'><a className='page-link boton2' href=''> Twitter</a></nav>
         <nav className='mb-3 boton1'><a className='page-link boton2' href='Whatsapp'>Whatsapp</a> </nav>
         <nav className='mb-3 boton1'><a className='page-link boton2' href='Facebook' >Facebook</a> </nav>
         <nav className='boton1'><a className='page-link boton2' type="button" href="https://play.howstuffworks.com/quiz/a-qu-casa-de-hogwarts-perteneces" >A que casa perteneces</a></nav>
