6f6aea37afe30fe25d49f503331caf14a0f05fb8
gonzalo <gonzalogabrielapu@gmail.com>
2023-12-06 20:10:43 -0300

    ortografia

--- src/app/(componentes)/generales/footer.tsx
+++ src/app/(componentes)/generales/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <footer className="finpagina">{/*le agrego el css */}
       <div className="container">
-        <p className="float-start"> Contacto Telefonico: 0-800-Hodwarts-111-999 </p>
+        <p className="float-start"> Contacto Telefonico: 0-800-Hogwarts-111-999 </p>
         <p className="float-end">Correo:<a className="link-opacity-50-hover"
           href="" target="blank">municipalidadhodward@gmail.com</a></p>{/*al correo lo hago un boton link */}
       </div>
